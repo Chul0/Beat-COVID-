d1c385b75d84523a689321849474f3d548d77acf
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-03-22 13:31:13 -0400

    6th commit

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ for (let i = 0; i < 10; i++) {
     document.querySelector('.game-board2').append(hole);
 }
 
+const winningMessage = () => `You saved the world!`;
+const losingMessage = () => `You were close!`
+const winningCondition = 100;
+
 let timeRemaining =START_TIMER;
 let score = 0 ; //score has to be changed so value is 0
 let tickId = null; //same goes for tick and mole
@@ -25,6 +29,7 @@ document.querySelector('.score-number').innerText=score;
 document.querySelector('.start-button').addEventListener('click', (event) => {
     document.querySelector('.game-board').classList.add('hidden');
     document.querySelector('.game-board2').classList.remove('hidden');
+    document.querySelector('.game-board3').classList.add('hidden');
    //  console.log('you cliked');
    const addVirus = (num) => {
         while(num > 0) {
@@ -39,7 +44,19 @@ document.querySelector('.start-button').addEventListener('click', (event) => {
             num--;
         }
     } 
+    const addVirus2 = () => {
+        setInterval(() => {
+            const allHoles = document.querySelectorAll('.hole');
+            const randomHole = Math.floor(Math.random() * allHoles.length);
+            allHoles[randomHole].classList.add('virus2');
+            setTimeout(() => {
+                allHoles[randomHole].classList.remove('virus2');
+            }, 5000)
+        }, 10000);
+    }
+    
     addVirus(3) //always invoke function!!
+    addVirus2(1)
 
     let interval = setInterval(()=> {
         document.querySelector('.time-number').innerText=timeRemaining;
@@ -49,6 +66,11 @@ document.querySelector('.start-button').addEventListener('click', (event) => {
              document.querySelector('.time-number').innerText='0';
             }
     }, 1000);
+        setTimeout(() => {
+            console.log("game over!")
+            document.querySelector('.game-board2').classList.add('hidden')
+            document.querySelector('.game-board3').classList.remove('hidden')
+        }, 31000)
 })    
 
 //for/of - loops through the values of an iterable object
@@ -56,11 +78,29 @@ for (hole of document.querySelectorAll('.hole')) {
     hole.addEventListener('click', (event) => {
         if(event.target.classList.contains('virus')){
             event.target.classList.remove('virus')
-            score ++;
+            score += 2;
+            document.querySelector('.score-number').innerText=score
+        } else if (event.target.classList.contains('virus2')){
+            event.target.classList.remove('virus2')
+            score += 5;
             document.querySelector('.score-number').innerText=score
         }
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // const removeVirus = () => {
     //     setTimeout(() => {
